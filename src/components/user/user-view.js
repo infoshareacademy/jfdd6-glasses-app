ff7e1899391e18df8f90fdf029dcd95757983cfa
natan-mlk <adam.milko@gmail.com>
2017-03-01 22:48:32 +0100

    User-view now shows content of all its inner components

--- src/components/user/user-view.js
+++ src/components/user/user-view.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import MapFeature from './map'
+import UserProfile from './user-profile.js'
+import UserFilmList from './user-film-list.js'
 
-const UserView = () => {
-
-}
+const UserView = () => (
+<div>
+    <UserProfile />
+    <UserFilmList />
+    <MapFeature />
+</div>
+)
 
 export default UserView
