176ae53e5dc9eb874c982dd70cd6f8d01c7fe8e9
Sylwia Rzedzicka <rzedzicka@gmail.com>
2017-03-01 22:43:23 +0100

    FD6GL-75 add components files

---
+++ src/components/movies/movies-search-bar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {FormControl} from 'react-bootstrap'
+
+const SearchBar = () => (
+  <div>
+    <form>
+      <FormControl type="text" placeholder="text" />
+    </form>
+  </div>
+);
+
+export default SearchBar
