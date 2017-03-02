d9d02bc06b61c5e24a9278981a889fb55b11b164
natan-mlk <adam.milko@gmail.com>
2017-03-02 20:44:29 +0100

    FD6GL-63 wip

--- src/components/user/user-profile.js
+++ src/components/user/user-profile.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Grid, Table} from 'react-bootstrap'
-import groups from '../../data/users.json'
+import users from '../../data/users.json'
 
 //[{"id":1,"first_name":"Andrew","last_name":"Greene","login":"agreene0","gender":"Male","avatar":"https://robohash.org/sitquiaearum.jpg?size=100x100&set=set1","description":"Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor.","postal":"8300","movies":[{},{},{},{}]},
 
@@ -8,17 +8,11 @@ const myUser = 3
 
 const GroupsView = () => (
   <Grid>
-    <h1>Groups</h1>
-    <Table striped>
-      <thead>
-      <tr>
-        <th>Id</th>
-        <th>Name</th>
-      </tr>
-      </thead>
-      <tbody>
+    <h1>User</h1>
+
+    <p>
       {
-        groups.map(
+        users.map(
           group => (
             <tr key={group.id}>
               <td>{group.id}</td>
@@ -27,8 +21,7 @@ const GroupsView = () => (
           )
         )
       }
-      </tbody>
-    </Table>
+    </p>
   </Grid>
 )
 
