eb0e6f0bc8255fed4b5ef2451ee56ac32a758aff
natan-mlk <adam.milko@gmail.com>
2017-03-02 20:41:53 +0100

    FD6GL-63 wip

--- src/components/user/user-profile.js
+++ src/components/user/user-profile.js
@@ -1,21 +1,35 @@
 import React from 'react'
-import {Grid} from 'react-bootstrap'
-import usersArray from '../../data/users.json'
+import {Grid, Table} from 'react-bootstrap'
+import groups from '../../data/users.json'
 
-const UserProfile = () => {
-  return (
-    <Grid>
+//[{"id":1,"first_name":"Andrew","last_name":"Greene","login":"agreene0","gender":"Male","avatar":"https://robohash.org/sitquiaearum.jpg?size=100x100&set=set1","description":"Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor.","postal":"8300","movies":[{},{},{},{}]},
 
-      {usersArray.map(
-        singleUser => (<p>{singleUser.first_name}</p>)
-      )}
+const myUser = 3
 
+const GroupsView = () => (
+  <Grid>
+    <h1>Groups</h1>
+    <Table striped>
+      <thead>
+      <tr>
+        <th>Id</th>
+        <th>Name</th>
+      </tr>
+      </thead>
+      <tbody>
+      {
+        groups.map(
+          group => (
+            <tr key={group.id}>
+              <td>{group.id}</td>
+              <td>{group.name}</td>
+            </tr>
+          )
+        )
+      }
+      </tbody>
+    </Table>
+  </Grid>
+)
 
-      <p style={{backgroundColor: 'pink'}}>
-        This is USER PROFILE
-      </p>
-    </Grid> )
-}
-
-
-export default UserProfile
+export default GroupsView
