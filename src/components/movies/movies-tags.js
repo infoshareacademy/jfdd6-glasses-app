839ebf1f97977d5fd5865fbe15959cf070e6cbfe
Sylwia Rzedzicka <rzedzicka@gmail.com>
2017-03-02 22:38:26 +0100

    FD6GL-75 add basic layout

--- src/components/movies/movies-tags.js
+++ src/components/movies/movies-tags.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import {Row, Col} from 'react-bootstrap'
+
+import './movies-style.css'
 
 const Tags = () => (
-  <div>tag</div>
+  <Col xs={10} xsOffset={1}>
+    <Row>
+      <Col className="movies-tags movies-border">
+        tags
+      </Col>
+    </Row>
+  </Col>
 )
 
 export default Tags
--- src/components/movies/movies-view.js
+++ src/components/movies/movies-view.js
@@ -2,16 +2,20 @@ import React from 'react'
 import SearchBar from './movies-search-bar'
 import Tags from './movies-tags'
 import MovieList from './movies-list'
+import {Grid} from 'react-bootstrap'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
+import './movies-style.css'
+
+
 const MoviesView = () => (
-  <div>
+  <Grid className="movies-container">
     <SearchBar />
     <Tags />
     <MovieList />
-  </div>
+  </Grid>
 )
 
 export default MoviesView
