63a86134317f32ff273d328a423bcfb65fcfed2b
natan-mlk <adam.milko@gmail.com>
2017-03-02 21:04:42 +0100

    FD6GL-63 user-profile now shows personal data, avatar and description

--- src/components/user/user-profile.js
+++ src/components/user/user-profile.js
@@ -2,26 +2,26 @@ import React from 'react'
 import {Grid, Table} from 'react-bootstrap'
 import users from '../../data/users.json'
 
-//[{"id":1,"first_name":"Andrew","last_name":"Greene","login":"agreene0","gender":"Male","avatar":"https://robohash.org/sitquiaearum.jpg?size=100x100&set=set1","description":"Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor.","postal":"8300","movies":[{},{},{},{}]},
-
-const myUser = 3
+const myUser = 2
 
 const GroupsView = () => (
   <Grid>
-    <h1>User</h1>
+    <h3>User:</h3>
+
+    <p><img src={users[myUser].avatar} /></p>
 
     <p>
-      {
-        users.map(
-          group => (
-            <tr key={group.id}>
-              <td>{group.id}</td>
-              <td>{group.name}</td>
-            </tr>
-          )
-        )
-      }
+      {users[myUser].first_name} {users[myUser].last_name}, {users[myUser].gender}
     </p>
+
+    <p>
+      Login: {users[myUser].login}
+    </p>
+
+    <p>
+      About {users[myUser].first_name}: {users[myUser].description}
+    </p>
+
   </Grid>
 )
 
