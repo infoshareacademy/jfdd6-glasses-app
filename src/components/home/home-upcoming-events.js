ad5d0c09c515932460e605e6e3299bb2699b4d4c
mitroc <michaltrocha@gmail.com>
2017-03-02 02:22:46 +0100

    Add draft bootstrap classes for modules.

--- src/components/home/home-upcoming-events.js
+++ src/components/home/home-upcoming-events.js
@@ -4,7 +4,14 @@
 import React from 'react'
 
 const HomeUpcomingEvents = () => (
-  <div>Pole najbilższych wydarzeń</div>
+  <div>
+    <h1>Pole najbilższych wydarzeń</h1>
+    <h1>Pole najbilższych wydarzeń</h1>
+    <h1>Pole najbilższych wydarzeń</h1>
+    <h1>Pole najbilższych wydarzeń</h1>
+    <h1>Pole najbilższych wydarzeń</h1>
+    <h1>Pole najbilższych wydarzeń</h1>
+  </div>
 );
 
 export default HomeUpcomingEvents
