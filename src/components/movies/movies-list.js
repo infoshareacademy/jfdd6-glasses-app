427ab0a284bd248ffc25303b5e0e7d094d19e8e2
Sylwia Rzedzicka <rzedzicka@gmail.com>
2017-03-02 17:04:20 +0100

    FD6GL-59 add simple table

--- src/components/movies/movies-list.js
+++ src/components/movies/movies-list.js
@@ -1,8 +1,25 @@
 import React from 'react'
-import {Grid} from 'react-bootstrap'
+import {Grid, Table} from 'react-bootstrap'
+import movies from '../../data/movies.json'
 
-const MovieList= () => (
-  <Grid>list</Grid>
-)
+const MovieList= () => {
+  console.log(movies)
+  return (
+    <Grid>
+      <Table bordered>
+        <tbody>
+        {movies.map(movie => (
+          <tr key={movie.id}>
+            <td><img src={movie.poster} alt={movie.name} /></td>
+            <td>{movie.name}</td>
+          </tr>
+        ))}
+
+        </tbody>
+      </Table>
+    </Grid>
+  )
+}
+
 
 export default MovieList
