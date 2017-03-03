40459041f1e8b8bc3087015b70f87e8830c76e81
Krzysztof Zielinski <krzysztof.zielinski.work@gmail.com>
2017-03-03 07:49:11 +0100

    feature/FD6GL-70-single-movie-page
                           experiments with layout, added users avatars

--- src/components/movie/movie-view.js
+++ src/components/movie/movie-view.js
@@ -1,30 +1,43 @@
 import React from 'react'
 import {Grid, Row, Col, Carousel} from 'react-bootstrap'
-import movies from "./data/movie-data.json"
+import movies from '../../data/movies.json'
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+
+
 
 
 const MovieView = () => (
 
   <Grid>
     <Row className="show-grid">
+      <Col xs={6} md={6}>
+      </Col>
+      <Col xs={6} md={6}>
+    <h2>Film {movies.name}</h2>
+      </Col></Row>
+
+    <Row className="show-grid">
       <Col xs={12} md={6}>
         <Carousel>
           <Carousel.Item>
-            <img width={900} height={500} alt="900x500" src="favicon.ico"/>
+            <img width={900} height={500} alt="900x500" src="http://lorempixel.com/900/900/abstract/1"/>
             <Carousel.Caption>
               <h3>First slide label</h3>
               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img width={900} height={500} alt="900x500" src="favicon.ico"/>
+            <img width={900} height={500} alt="900x500" src="http://lorempixel.com/900/900/abstract/2"/>
             <Carousel.Caption>
               <h3>Second slide label</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img width={900} height={500} alt="900x500" src="favicon.ico"/>
+            <img width={900} height={500} alt="900x500" src="http://lorempixel.com/900/900/abstract/3"/>
             <Carousel.Caption>
               <h3>Third slide label</h3>
               <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
@@ -38,30 +51,33 @@ const MovieView = () => (
 
         {
           movies.filter(
-            movie => movie.opis.length > 30
+            movie => movie.description.length < 250
           ).map(
             movie => (
-              <p key={movie.id}>{movie.opis}</p>
+              <p key={movie.id}>{movie.description}</p>
             )
           )
         }
       </Col>
     </Row>
     <Row className="show-grid">
-      <Col md={6} mdPush={6}>
+      <Col md={12}>
+        <h2>Lista użytkowników, ktorzy mają ten film i chętnie umówią się na wspólny seans:</h2>
+      </Col></Row>
 
+    <Row className="show-grid">
 
-      </Col>
-      <Col md={6} mdPull={6}>
+      <Col md={12}>
 
         <table>
           <tbody>
-
           <tr>
             {
-              movies.map(
+              movies.filter(
+                movie => movie.id < 5
+              ).map(
                 movie => (
-                  <td key={movie.id}>{movie.name}</td>
+                  <td key={movie.id}><img src={movie.poster} /></td>
                 )
               )
             }
@@ -70,6 +86,12 @@ const MovieView = () => (
           </tbody></table>
       </Col>
     </Row>
+    <Row className="show-grid">
+      <Col xs={6} md={6}>
+      </Col>
+      <Col xs={6} md={6}>
+        <h2>Footer</h2>
+      </Col></Row>
   </Grid>
 
 );
