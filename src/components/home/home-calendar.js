ae9aba7fc635e8983c1c4fdd1e15e12b34f484ec
mitroc <michaltrocha@gmail.com>
2017-03-03 16:51:48 +0100

    FD6GL-54 Events import to calendar ok.

--- src/components/home/home-calendar.js
+++ src/components/home/home-calendar.js
@@ -5,6 +5,7 @@ import React from 'react'
 
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
+import events from '../../data/events-js'
 
 // Setup the localizer by providing the moment (or globalize) Object
 // to the correct localizer.
@@ -13,9 +14,7 @@ BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
 const HomeCalendar = (props) => (
   <div style={{height: 400}}>
     <BigCalendar
-      events={[]}
-      startAccessor='startDate'
-      endAccessor='endDate'
+      events={events}
     />
   </div>
 );
