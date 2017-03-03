26e8e50bc21a67e9cfc04b8170f7a83cb91b1983
Sylwia Rzedzicka <rzedzicka@gmail.com>
2017-03-03 20:24:58 +0100

    FD6GL-58 display tag buttons

--- src/components/movies/movies-tags.js
+++ src/components/movies/movies-tags.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import {Row, Col} from 'react-bootstrap'
+import {Row, Col, Button} from 'react-bootstrap'
+import tags from '../../data/tags.json'
 
 const Tags = () => (
   <Col xs={10} xsOffset={1}>
     <Row>
       <Col className="movies-tags movies-border">
-        tags
+        {tags.map((tag, index) => (
+          <Button key={tag.id} bsSize="small" className="movies-tag-button">{tag.name}</Button>
+          )
+        )}
       </Col>
     </Row>
   </Col>
