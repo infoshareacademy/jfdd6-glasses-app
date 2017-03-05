bb970155f3703069f5665a61068e8c2acf82de17
Krzysztof Zielinski <krzysztof.zielinski.work@gmail.com>
2017-03-05 17:23:14 +0100

    feature/FD6GL-70-single-movie-description module code cleanup, added search module file

---
+++ src/components/movie/movie-search.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MovieSearch = () => (
+  <div><p>Wyszukiwarka <input type="text" /></p></div>
+)
+
+export default MovieSearch
--- src/components/movie/movie-view.js
+++ src/components/movie/movie-view.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Grid, Row, Col} from 'react-bootstrap'
-import movies from '../../data/movies.json'
+import MovieSearch from './movie-search'
 import MovieCarousel from './movie-carousel'
 import MovieDescription from './movie-description'
 import UserList from './movie-user-list'
@@ -8,64 +8,42 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
 
-
-
-
 const MovieView = () => (
-
   <Grid>
     <Row className="show-grid">
-      <Col xs={6} md={6}>
+      <Col xs={12} md={12}>
+        <MovieSearch/>
       </Col>
-      <Col xs={6} md={6}>
-    <h2>Film {movies.name}</h2>
-      </Col></Row>
-
+    </Row>
+    <Row>
+      <Col xs={12} md={12}>
+        <h2>Tytuł filmu: </h2>
+      </Col>
+    </Row>
     <Row className="show-grid">
       <Col xs={12} md={6}>
-
-<MovieCarousel />
-
+        <MovieCarousel />
       </Col>
       <Col xs={6} md={6}>
+        <h2>Tytuł filmu</h2>
         <MovieDescription />
       </Col>
     </Row>
     <Row className="show-grid">
       <Col md={12}>
         <h2>Lista użytkowników, ktorzy mają ten film i chętnie umówią się na wspólny seans:</h2>
-      </Col></Row>
-
-    <Row className="show-grid">
-
-      <Col md={12}>
-
         <table>
           <tbody>
-          <tr>
-            {
-              movies.filter(
-                movie => movie.id < 5
-              ).map(
-                movie => (
-                  <td key={movie.id}><img src={movie.poster} alt="" /></td>
-                )
-              )
-            }
-
-          </tr>
           <UserList/>
-          </tbody></table>
+          </tbody>
+        </table>
       </Col>
     </Row>
     <Row className="show-grid">
-      <Col xs={6} md={6}>
-      </Col>
-      <Col xs={6} md={6}>
+      <Col xs={12} md={12}>
         <h2>Footer</h2>
       </Col></Row>
   </Grid>
-
 );
 
 export default MovieView
