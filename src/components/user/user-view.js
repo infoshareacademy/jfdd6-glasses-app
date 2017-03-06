c06ed7480cb74782954823344ecf22a4577e0ea2
Adam Milko <adam.milko@gmail.com>
2017-03-06 16:43:42 +0100

    FD6GL-64 code cleaning

--- src/components/user/user-view.js
+++ src/components/user/user-view.js
@@ -6,26 +6,11 @@ import UserFilmList from './user-film-list.js'
 
 
 const UserView = (props) => {
-
   const userId = props.params.userId
-  console.log(userId)
+
   return (
     <div>
-      <UserProfile userId={props.params.userId}/>
-
-
-     {/* {
-        props.groups.filter(
-          grupa =>
-          grupa.id === parseInt(props.params.groupId, 10) //te 10 to z definicji metody parseInt
-        ).map(
-          grupa => (
-            <p key={grupa.id}>{grupa.name}</p>
-          )
-        )
-      }*/}
-
-
+      <UserProfile id={userId}/>
       <UserFilmList id={userId}/>
       <MapFeature />
     </div>
