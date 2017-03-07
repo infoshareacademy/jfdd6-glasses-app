7b8ec1a0688380637e0237d9b1b8ea56e38ad9ff
mitroc <michaltrocha@gmail.com>
2017-03-07 09:54:32 +0100

    FD6GL-54 Dates formatted properly.

--- src/components/home/home-upcoming-events.js
+++ src/components/home/home-upcoming-events.js
@@ -5,6 +5,34 @@ import { Table } from 'react-bootstrap'
 const HomeUpcomingEvents = ({events}) => {
   const limit = 8
 
+  const eventsImproved = events.slice().sort(
+    (prevEvent, nextEvent) =>(
+      new Date(nextEvent.start) - new Date(prevEvent.start)
+    )).slice(0, limit).map(
+    event => ({
+      ...event,
+      startYear: new Date(event.start).getFullYear(),
+      startMonth:
+        (new Date(event.start).getMonth()) >= 10 ?
+          new Date(event.start).getMonth() :
+            '0' + new Date(event.start).getMonth(),
+      startDay:
+        (new Date(event.start).getDate()) >= 10 ?
+          new Date(event.start).getDate() :
+            '0' + new Date(event.start).getDate(),
+
+      startHour:
+        (new Date(event.start).getHours()) >= 10 ?
+          new Date(event.start).getHours() :
+            '0' + new Date(event.start).getHours(),
+
+      startMinute:
+        (new Date(event.start).getMinutes()) >=10 ?
+          new Date(event.start).getMinutes() :
+            '0' + new Date(event.start).getMinutes()
+    })
+  )
+
   return (
   <div>
     <h2>Wydarzenia</h2>
@@ -17,19 +45,16 @@ const HomeUpcomingEvents = ({events}) => {
       </thead>
         <tbody>
         {
-          events.sort(
-            (prevEvent ,nextEvent) => (
-              new Date(nextEvent.start) - new Date(prevEvent.start)
-            )).slice(0, limit).map(
+          eventsImproved.map(
             event => (
             <tr key={event.id}>
               <td>{event.title}</td>
               <td>
-                {new Date(event.start).getFullYear()}-
-                {new Date(event.start).getMonth()}-
-                {new Date(event.start).getDate() + ' '}
-                {new Date(event.start).getHours()}:
-                {new Date(event.start).getMinutes()}
+                {event.startYear + '-'}
+                {event.startMonth + '-'}
+                {event.startDay + ' '}
+                {event.startHour + ':'}
+                {event.startMinute}
               </td>
             </tr>
             )
@@ -39,6 +64,7 @@ const HomeUpcomingEvents = ({events}) => {
     </Table>
   </div>
 )}
+
 
 export default connect(
   state => ({
