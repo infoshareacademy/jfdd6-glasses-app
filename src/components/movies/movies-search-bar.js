8fd0136d7c32edcf873c49a846bf45e18f887c69
Sylwia Rzedzicka <rzedzicka@gmail.com>
2017-03-07 15:16:31 +0100

    FD6GL-57 display title hints under search bar

--- src/components/movies/movies-search-bar.js
+++ src/components/movies/movies-search-bar.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router'
 import QueryButton from './movies-search'
 import { Col, FormGroup, InputGroup, FormControl } from 'react-bootstrap'
 
-const SearchBar = ({ fieldValue, createQuery }) => (
-  <Col xs={8} xsOffset={2}>
-    <FormGroup>
+const SearchBar = ({ fieldValue, createQuery, movies }) => (
+  <Col xs={8} xsOffset={2} className="movies-position-hints">
+    <FormGroup className="movies-stick-hints">
       <InputGroup>
         <FormControl
+          id="search"
           type="text"
           value={fieldValue}
           onChange={(event) => createQuery(event.target.value)}
@@ -19,12 +21,26 @@ const SearchBar = ({ fieldValue, createQuery }) => (
         </InputGroup.Button>
       </InputGroup>
     </FormGroup>
+    <ul id="hints" className="movies-search-hints">
+      {movies
+        .filter(movie =>
+          fieldValue.length > 0 ? movie.name.toLowerCase().indexOf(fieldValue.toLowerCase()) !== -1 : false
+        )
+        .slice(0,10)
+        .map(movie =>
+          <li key={movie.id}>
+            <Link to={'/movie/' + movie.id}>{movie.name}</Link>
+          </li>
+        )
+      }
+    </ul>
   </Col>
 )
 
 export default connect(
   state => ({
-    fieldValue: state.movies.query
+    fieldValue: state.movies.query,
+    movies: state.movies.moviesData
   }),
   dispatch => ({
     createQuery: (value) => dispatch({ type: 'movies/search/QUERY', value })
