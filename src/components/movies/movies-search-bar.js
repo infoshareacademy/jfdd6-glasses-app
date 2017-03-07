3ef0a301566de536d2f026767864f6aec832e936
Sylwia Rzedzicka <rzedzicka@gmail.com>
2017-03-07 17:03:44 +0100

    FD6GL-57 wip: hide/show hints

--- src/components/movies/movies-search-bar.js
+++ src/components/movies/movies-search-bar.js
@@ -4,38 +4,55 @@ import { Link } from 'react-router'
 import QueryButton from './movies-search'
 import { Col, FormGroup, InputGroup, FormControl } from 'react-bootstrap'
 
-const SearchBar = ({ fieldValue, createQuery, movies }) => (
-  <Col xs={8} xsOffset={2} className="movies-position-hints">
-    <FormGroup className="movies-stick-hints">
-      <InputGroup>
-        <FormControl
-          id="search"
-          type="text"
-          value={fieldValue}
-          onChange={(event) => createQuery(event.target.value)}
-          placeholder="wyszukaj"
-        />
+class SearchBar extends React.Component {
 
-        <InputGroup.Button>
-          <QueryButton />
-        </InputGroup.Button>
-      </InputGroup>
-    </FormGroup>
-    <ul id="hints" className="movies-search-hints">
-      {movies
-        .filter(movie =>
-          fieldValue.length > 0 ? movie.name.toLowerCase().indexOf(fieldValue.toLowerCase()) !== -1 : false
-        )
-        .slice(0,10)
-        .map(movie =>
-          <li key={movie.id}>
-            <Link to={'/movie/' + movie.id}>{movie.name}</Link>
-          </li>
-        )
-      }
-    </ul>
-  </Col>
-)
+  keyDown(event) {
+    if(event){
+      console.log('keydown-focus')
+    }
+  }
+
+
+  render() {
+    const {fieldValue, createQuery, movies} = this.props
+
+    return (
+      <Col xs={8} xsOffset={2} className="movies-position-hints">
+        <FormGroup className="movies-stick-hints">
+          <InputGroup>
+            <FormControl
+              ref="search"
+              id="search"
+              type="text"
+              value={fieldValue}
+              onChange={(event) => createQuery(event.target.value)}
+              onBlur={() => console.log('blur')}
+              onKeyDown={this.keyDown}
+              placeholder="wyszukaj"
+            />
+
+            <InputGroup.Button>
+              <QueryButton />
+            </InputGroup.Button>
+          </InputGroup>
+        </FormGroup>
+        <ul id="hints" className="movies-search-hints">
+          {movies
+            .filter(movie =>
+              fieldValue.length > 0 ? movie.name.toLowerCase().indexOf(fieldValue.toLowerCase()) !== -1 : false
+            )
+            .slice(0, 10)
+            .map(movie =>
+              <li key={movie.id}>
+                <Link to={'/movie/' + movie.id}>{movie.name}</Link>
+              </li>
+            )
+          }
+        </ul>
+      </Col>
+    )
+  }
+}
 
 export default connect(
   state => ({
