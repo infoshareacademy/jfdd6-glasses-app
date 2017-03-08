7ff54e25575b800977b4d5030f7c13c56ebdb559
natan-mlk <adam.milko@gmail.com>
2017-03-08 20:10:50 +0100

    FD6GL-48: list of all-users - code formatting

--- src/components/users/all-users.js
+++ src/components/users/all-users.js
@@ -3,24 +3,24 @@ import {Table, Grid} from 'react-bootstrap'
 import {connect} from 'react-redux'
 import {Link} from 'react-router'
 
-const AllUsers = ({users}) => {
-
-  return(
+const AllUsers = ({users}) =>
+  (
     <Grid>
       <h1>Wszyscy użytkownicy</h1>
       <Table striped bordered>
         <tbody>
         {users.map(
-          (arg) => <tr key={arg.id}><td><Link to={'/user/' + arg.id}>{arg.first_name} {arg.last_name}</Link></td></tr>
-            )}
+          (arg) => <tr key={arg.id}>
+            <td><Link to={'/user/' + arg.id}>{arg.first_name} {arg.last_name}</Link></td>
+          </tr>
+        )}
         </tbody>
       </Table>
     </Grid>
   )
-}
 
-  export default connect(
-    state => ({
-      users: state.user.userData,
-    })
-  )(AllUsers)
+export default connect(
+  state => ({
+    users: state.user.userData,
+  })
+)(AllUsers)
