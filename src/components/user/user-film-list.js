e9a12d24924d151695f4b80d0a90df444de037a7
Adam Milko <adam.milko@gmail.com>
2017-03-08 16:47:16 +0100

    FD6GL-64: code cleaning

--- src/components/user/user-film-list.js
+++ src/components/user/user-film-list.js
@@ -2,11 +2,6 @@ import React from 'react'
 import {Grid, Table} from 'react-bootstrap'
 import { connect} from 'react-redux'
 import { Link } from 'react-router'
-//import users from '../../data/users.json'
-//import titles from '../../data/movies.json'
-
-
-
 
 const UserFilmList = ({id, users, moviesList}) => {
 
@@ -15,16 +10,12 @@ const UserFilmList = ({id, users, moviesList}) => {
    return (
   <Grid>
     <h3>Lista filmów użytkownika: {filteredUser.login}</h3>
-
-
     <Table striped>
       <thead>
       <tr>
         <th>Jakie ma filmy</th>
       </tr>
       </thead>
-
-
       <tbody>
       {
         moviesList.filter(
@@ -38,10 +29,9 @@ const UserFilmList = ({id, users, moviesList}) => {
     </Grid>
 )}
 
-//export default UserFilmList
 export default connect(
   state => ({
-    users: state.user.userData, //wyciągam ze stanu aplikacji listę userów pobraną z Jsonów
-    moviesList: state.movies.moviesData //wyciągam ze stanu aplikacji listę filmów pobraną z Jsonów
+    users: state.user.userData,
+    moviesList: state.movies.moviesData
   })
 )(UserFilmList)
