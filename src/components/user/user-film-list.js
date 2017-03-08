fdc4955768a1fcfaa6d89a6994b8ccdb27b9f0c5
Adam Milko <adam.milko@gmail.com>
2017-03-08 16:54:41 +0100

    FD6GL-64: code cleaning

--- src/components/user/user-film-list.js
+++ src/components/user/user-film-list.js
@@ -1,33 +1,36 @@
 import React from 'react'
 import {Grid, Table} from 'react-bootstrap'
-import { connect} from 'react-redux'
-import { Link } from 'react-router'
+import {connect} from 'react-redux'
+import {Link} from 'react-router'
 
 const UserFilmList = ({id, users, moviesList}) => {
 
   const filteredUser = users.find(arg => arg.id === parseInt(id, 10))
 
-   return (
-  <Grid>
-    <h3>Lista filmów użytkownika: {filteredUser.login}</h3>
-    <Table striped>
-      <thead>
-      <tr>
-        <th>Jakie ma filmy</th>
-      </tr>
-      </thead>
-      <tbody>
-      {
-        moviesList.filter(
-          title => filteredUser.movies.indexOf(title.id) !== -1
-        ).map(
-          userTitle => <tr key={userTitle.id}><td><Link to={'/movie/' + userTitle.id}>{userTitle.name}</Link></td></tr>
-        )
-      }
-      </tbody>
-    </Table>
+  return (
+    <Grid>
+      <h3>Lista filmów użytkownika: {filteredUser.login}</h3>
+      <Table striped>
+        <thead>
+        <tr>
+          <th>Jakie ma filmy</th>
+        </tr>
+        </thead>
+        <tbody>
+        {
+          moviesList.filter(
+            title => filteredUser.movies.indexOf(title.id) !== -1
+          ).map(
+            userTitle => <tr key={userTitle.id}>
+              <td><Link to={'/movie/' + userTitle.id}>{userTitle.name}</Link></td>
+            </tr>
+          )
+        }
+        </tbody>
+      </Table>
     </Grid>
-)}
+  )
+}
 
 export default connect(
   state => ({
