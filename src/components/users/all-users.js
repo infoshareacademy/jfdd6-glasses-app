ca4e427e5a63a4fc01eff14f6cbaf09f1bbcbfdb
natan-mlk <adam.milko@gmail.com>
2017-03-08 20:08:48 +0100

    FD6GL-48: list of all-users with links to their profiles - using redux

--- src/components/users/all-users.js
+++ src/components/users/all-users.js
@@ -1,16 +1,26 @@
 import React from 'react'
-import {Table} from 'react-bootstrap'
+import {Table, Grid} from 'react-bootstrap'
+import {connect} from 'react-redux'
+import {Link} from 'react-router'
 
-const AllUsers = () => (
-  <div>
-    <h1>Wszyscy użytkownicy</h1>
-    <Table striped bordered>
-      <tbody>
+const AllUsers = ({users}) => {
 
+  return(
+    <Grid>
+      <h1>Wszyscy użytkownicy</h1>
+      <Table striped bordered>
+        <tbody>
+        {users.map(
+          (arg) => <tr key={arg.id}><td><Link to={'/user/' + arg.id}>{arg.first_name} {arg.last_name}</Link></td></tr>
+            )}
+        </tbody>
+      </Table>
+    </Grid>
+  )
+}
 
-      </tbody>
-    </Table>
-  </div>
-)
-
-export default AllUsers
+  export default connect(
+    state => ({
+      users: state.user.userData,
+    })
+  )(AllUsers)
