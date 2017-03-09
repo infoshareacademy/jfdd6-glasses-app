f6d86fac7f286db14bbd3b8147af96e45d1f7930
Adam Milko <adam.milko@gmail.com>
2017-03-09 14:58:07 +0100

    FD6GL-48: renaming

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -22,7 +22,7 @@ const App = (props) => (
           <LinkContainer to="/movies">
             <NavItem eventKey={2}>Lista filmów</NavItem>
           </LinkContainer>
-          <LinkContainer to="/user/:userId">
+          <LinkContainer to="/user/3">
             <NavItem eventKey={3}>Sąsiad</NavItem>
           </LinkContainer>
           <LinkContainer to="/users">
