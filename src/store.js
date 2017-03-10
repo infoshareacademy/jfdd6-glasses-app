e328bab0d8e05f78fd3a71b469210b23d3624197
Sylwia Rzedzicka <rzedzicka@gmail.com>
2017-03-10 15:54:32 +0100

    implement redux-thunk

--- src/store.js
+++ src/store.js
@@ -1,20 +1,28 @@
-import {createStore, combineReducers} from 'redux'
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 
 import moviesReducer from './state/movies'
 import movieReducer from './state/movie'
 import userReducer from './state/user'
-import eventsReducer from './state/events'
+// import homeEventsReducer from './state/home'
+// import homeFiltersReducer from './state/home-filters'
 
 const reducer = combineReducers({
   movies: moviesReducer,
   user: userReducer,
-  movie: movieReducer,
-  events: eventsReducer
+  movie: movieReducer
+  // home: homeEventsReducer,
+  // homeFilters: homeFiltersReducer
 })
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  /* preloadedState, */
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
 )
 
 export default store
