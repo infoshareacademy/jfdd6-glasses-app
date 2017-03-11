ecd735dfe21461d7989a6cd3af4488be2fb6c61a
natan-mlk <adam.milko@gmail.com>
2017-03-11 17:59:35 +0100

    FD6GL-76: ajax for single-user-page - WIP

--- src/components/user/user-profile.js
+++ src/components/user/user-profile.js
@@ -1,40 +1,42 @@
 import React from 'react'
 import {Grid} from 'react-bootstrap'
 import { connect} from 'react-redux'
-import users from '../../data/users.json'
+//import users from '../../data/users.json'
 
 
 
+class UserProfile extends React.Component {
 
-const GroupsView = (props) => {
+  render() {
 
-  const filteredUser = users.find(user => user.id === parseInt(props.id, 10))
+    const {id, users} = this.props;
+    const filteredUser = users.data ? users.data.find(user => user.id === parseInt(id, 10)) : users.data[0];
 
-  return (
-    <Grid>
-      <h3>User:</h3>
+    return (
+      <Grid>
+        <h3>User:</h3>
 
-      <p><img src={filteredUser.avatar} alt="avatar"/></p>
+        <p><img src={filteredUser.avatar} alt="avatar"/></p>
 
-      <p>
-        {filteredUser.first_name} {filteredUser.last_name}, {filteredUser.gender}
-      </p>
+        <p>
+          {filteredUser.first_name} {filteredUser.last_name}, {filteredUser.gender}
+        </p>
 
-      <p>
-        Login: {filteredUser.login}
-      </p>
+        <p>
+          Login: {filteredUser.login}
+        </p>
 
-      <p>
-        About {filteredUser.first_name}: {filteredUser.description}
-      </p>
+        <p>
+          About {filteredUser.first_name}: {filteredUser.description}
+        </p>
 
-    </Grid>
-  )
+      </Grid>
+    )
+  }
 }
-
 
 export default connect(
   state => ({
-    user: state.user.userData //wyciągam ze stanu aplikacji listę
+    users: state.user //wyciągam ze stanu aplikacji listę
   })
-)(GroupsView)
+)(UserProfile)
