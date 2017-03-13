fb186a04e8ef40f1a59008786d337c5f2679602b
Krzysztof Zieliński <krzysztof.zielinski.work@gmail.com>
2017-03-13 13:43:44 +0100

    removed unused elements from view

--- src/components/movie/movie-carousel.js
+++ src/components/movie/movie-carousel.js
@@ -7,22 +7,22 @@ const MovieCarousel = () => (
     <Carousel.Item>
       <img width={900} height={500} alt="900x500" src="http://lorempixel.com/900/900/abstract/1"/>
       <Carousel.Caption>
-        <h3>First slide label</h3>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        {/*<h3>First slide label</h3>*/}
+        {/*<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>*/}
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
       <img width={900} height={500} alt="900x500" src="http://lorempixel.com/900/900/abstract/2"/>
       <Carousel.Caption>
-        <h3>Second slide label</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        {/*<h3>Second slide label</h3>*/}
+        {/*<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>*/}
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
       <img width={900} height={500} alt="900x500" src="http://lorempixel.com/900/900/abstract/3"/>
       <Carousel.Caption>
-        <h3>Third slide label</h3>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        {/*<h3>Third slide label</h3>*/}
+        {/*<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>*/}
       </Carousel.Caption>
     </Carousel.Item>
   </Carousel>
