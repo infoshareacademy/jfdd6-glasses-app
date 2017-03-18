c66c2eb24b1521bbd849d4cbf4494e27525940cc
Sylwia Rzedzicka <rzedzicka@gmail.com>
2017-03-18 17:00:12 +0100

    FD6GL-59 implement new layout

--- src/components/movies/movies-list.js
+++ src/components/movies/movies-list.js
@@ -1,44 +1,41 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
-import { Table } from 'react-bootstrap'
 
 const MovieList = ({movies, customTags, query, queryTag}) => (
-      <Table bordered className="movies-list film-table">
-        <tbody>
-        {
-          movies.length === 0
-            ?
-            <tr>
-              <td>Ładowanie&hellip;</td>
-            </tr>
-            :
-            movies
-              .filter(movie =>
-                movie.name.toLowerCase().indexOf(query.toLowerCase()) !== -1
-              )
-              .filter(movie =>
-                queryTag > 0 ? movie.tags.indexOf(queryTag) !== -1 : true
-              )
-              .filter(movie =>
-                customTags.every(tag =>
-                  movie.tags.indexOf(tag) !== -1
-                )
-              )
-              .map(movie => (
-                  <tr key={movie.id}>
-                    <td><img src={movie.poster} alt={movie.name}/></td>
-                    <td>
-                      <Link to={'/movie/' + movie.id}>
-                        {movie.name}
-                      </Link>
-                    </td>
-                  </tr>
-                )
-              )
-        }
-        </tbody>
-      </Table>
+  <div>
+    {
+      movies.length === 0
+        ?
+        <p>Ładowanie…</p>
+        :
+        movies
+          .filter(movie =>
+            movie.name.toLowerCase().indexOf(query.toLowerCase()) !== -1
+          )
+          .filter(movie =>
+            queryTag > 0 ? movie.tags.indexOf(queryTag) !== -1 : true
+          )
+          .filter(movie =>
+            customTags.every(tag =>
+              movie.tags.indexOf(tag) !== -1
+            )
+          )
+          .map(movie => (
+              <div key={movie.id} className="movies-list-movie col-xs-12 col-sm-6 col-lg-4 movies-no-padding">
+                <div className="movies-spaced-list">
+                  <Link to={'/movie/' + movie.id}>
+                    <img src={movie.pics[0]} alt={movie.name}/>
+                    {movie.name}
+                    <br/>
+                    <span className="movies-original-title">({movie.originalTitle})</span>
+                  </Link>
+                </div>
+              </div>
+            )
+          )
+    }
+  </div>
 )
 
 export default connect(
