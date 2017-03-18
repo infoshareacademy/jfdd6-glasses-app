7bf475d7639d9deee43f88d5f7f71cb43c879f57
Sylwia Rzedzicka <rzedzicka@gmail.com>
2017-03-18 13:32:56 +0100

    FD6GL-58 add functionality to hide/show tags

--- src/components/movies/movies-tags.js
+++ src/components/movies/movies-tags.js
@@ -2,40 +2,79 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Button } from 'react-bootstrap'
 
-const Tags = ({ tags, customTags, sendTag, removeTag, resetTags}) => (
-  <div className="movies-tags movies-border">
-    {
-      tags.length === 0
-        ? <p>Ładowanie&hellip;</p>
-        :
-        tags.map((tag) => (
-            customTags.indexOf(tag.id) === -1
-              ? <Button
-                key={tag.id}
-                value={tag.id}
-                bsSize="small"
-                className="movies-tag-button"
-                onClick={() => sendTag(tag.id)}
-              >{tag.name}</Button>
-              : <Button
-                key={tag.id}
-                value={tag.id}
-                bsStyle="success"
-                bsSize="small"
-                className="movies-tag-button movies-tag-button-active"
-                onClick={() => removeTag(tag.id)}
-              >{tag.name}</Button>
-          )
-        )
-    }
-    <Button
-      bsStyle="info"
-      bsSize="small"
-      className="movies-tag-button movies-tag-button-reset"
-      onClick={() => resetTags()}
-    >Usuń filtry</Button>
-  </div>
-)
+class Tags extends React.Component {
+
+  showTags() {
+    document.getElementById('tags-list').style.display = 'block'
+    document.getElementById('tags-turn-on').style.display = 'none'
+    document.getElementById('tags-turn-off').style.display = 'block'
+  }
+
+  hideTags() {
+    document.getElementById('tags-list').style.display = 'none'
+    document.getElementById('tags-turn-on').style.display = 'block'
+    document.getElementById('tags-turn-off').style.display = 'none'
+  }
+
+  render() {
+    const {tags, customTags, sendTag, removeTag, resetTags} = this.props
+
+    return (
+      <div className="movies-tags">
+        <div id="tags-turn-on">
+          <Button
+            bsStyle="info"
+            bsSize="small"
+            className="movies-tag-button movies-tag-button-reset"
+            onClick={this.showTags}
+          >Pokaż filtry</Button>
+        </div>
+
+        <div id="tags-turn-off">
+          <Button
+            bsStyle="info"
+            bsSize="small"
+            className="movies-tag-button movies-tag-button-reset"
+            onClick={this.hideTags}
+          >Ukryj filtry</Button>
+        </div>
+
+        <div id="tags-list">
+          {
+            tags.length === 0
+              ? <p>Ładowanie&hellip;</p>
+              :
+              tags.map((tag) => (
+                  customTags.indexOf(tag.id) === -1
+                    ? <Button
+                      key={tag.id}
+                      value={tag.id}
+                      bsSize="small"
+                      className="movies-tag-button"
+                      onClick={() => sendTag(tag.id)}
+                    >{tag.name}</Button>
+                    : <Button
+                      key={tag.id}
+                      value={tag.id}
+                      bsStyle="success"
+                      bsSize="small"
+                      className="movies-tag-button movies-tag-button-active"
+                      onClick={() => removeTag(tag.id)}
+                    >{tag.name}</Button>
+                )
+              )
+          }
+          <Button
+            bsStyle="info"
+            bsSize="small"
+            className="movies-tag-button movies-tag-button-reset"
+            onClick={() => resetTags()}
+          >Usuń wybrane filtry</Button>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default connect(
   state => ({
