55bba4a8b7801b96e5bfc2f30f43c74c6585dfa2
Krzysztof Zielinski <krzysztof.zielinski.work@gmail.com>
2017-03-25 20:36:22 +0100

    feature/FD6GL-70-single-movie-description module added favicon, fixed movie.json and carousel paths

--- src/components/movie/movie-carousel.js
+++ src/components/movie/movie-carousel.js
@@ -15,7 +15,7 @@ class MovieCarousel extends React.Component {
                 movie => movie.id === parseInt(id, 10)
               ).map(
                 movie => (
-                  <img className="movie-img" alt={movie.name + ' poster'} src={movie.pics[1]} key={movie.id}/>
+                  <img className="movie-img" alt={movie.name + ' poster'} src={movie.pics[0]} key={movie.id}/>
                 )
               ) : <img className="movie-img" alt={movie.name + ' poster'} src="http://pipsum.com/320x440.jpg"/>
           }
@@ -30,7 +30,7 @@ class MovieCarousel extends React.Component {
                 movie => movie.id === parseInt(id, 10)
               ).map(
                 movie => (
-                  <img className="movie-img" alt={movie.name + ' poster'} src={movie.pics[0]} key={movie.id}/>
+                  <img className="movie-img" alt={movie.name + ' poster'} src={movie.pics[1]} key={movie.id}/>
 
                 )
               ) : <img className="movie-img" alt={movie.name + ' poster'} src="http://lorempixel.com/320/440/sports/"/>
