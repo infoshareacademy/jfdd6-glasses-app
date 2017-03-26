900fd78ee253657d22542bc2f8ab642bda18bb54
mitroc <michaltrocha@gmail.com>
2017-03-26 16:29:11 +0200

    FD6GL-69 Work in progress.

--- src/components/home/home-events.js
+++ src/components/home/home-events.js
@@ -74,87 +74,91 @@ const HomeEvents = ({events, start, change}) => {
                 </Col>
               </Row>
               <HomeInfo
-              classRow="home-event-panel"
-              classH3="home-event-title"
-              classH4="home-event-description"
-              title="Hmmm... Spróbuj wyszukać jeszcze raz."
-              description="Zmień Twoją lokalizację lub odległość."
-            />
-            </div>:
-            events.slice(
-              start, start + step
-            ).map(
-              event => (
-                <div>
-                  <Row className="home-btn-wrapper">
-                    <ButtonGroup>
-                      <Button
-                        id="home-btn-left"
-                        bsSize="small"
-                        bsStyle="info"
-                        className="movies-tag-button-reset"
-                        onClick={() => change(-step, events)}>
-                        &lt;
-                      </Button>
-                      <Button
-                        id="home-btn-right"
-                        bsSize="small"
-                        bsStyle="info"
-                        className="movies-tag-button-reset"
-                        onClick={() => change(step, events)}
-                      >
-                        &gt;
-                      </Button>
-                    </ButtonGroup>
-                  </Row>
-                  <Row className="home-event-panel-header">
-                    <Col xs={12}>
-                      <h5 className="home-event-description text-center">
-                        NADCHODZĄCE PROJEKCJE
-                      </h5>
-                    </Col>
-                  </Row>
-                  <Link
-                    key={event.id}
-                    to={"/event/" + event.id}>
-                    <Row className="home-event-panel">
-                      <Col xs={12} key={event.id}>
-                        <h3 className="home-event-title rbc-ellipsis">{event.movieTitle}</h3>
-                        <Row className="home-centered-vert">
-                          <Col xs={3}>
-                            <Image
-                              src={event.moviePicture}
-                              alt={event.movieTitle}
-                              responsive
-                            />
-                          </Col>
-                          <Col
-                            className="home-event-description"
-                            xs={9}>
-                            <h4>
-                              <Glyphicon glyph="time"/> {' '}
-                              {moment(event.start).format('dddd, D MMMM, H:mm')}
-                            </h4>
-                            <h4>
-                              <Glyphicon glyph="map-marker"/>
-                              {' '}
-                              {event.distance} m
-                            </h4>
-                            <div>
-                              <h4 >
-                                <Glyphicon glyph="bullhorn"/>
-                                &nbsp;
+                classRow="home-event-panel"
+                classH3="home-event-title"
+                classH4="home-event-description"
+                title="Hmmm... Spróbuj wyszukać jeszcze raz."
+                description="Zmień Twoją lokalizację lub odległość."
+              />
+            </div> :
+            <div>
+              <Row className="home-btn-wrapper">
+                <ButtonGroup>
+                  <Button
+                    id="home-btn-left"
+                    bsSize="small"
+                    bsStyle="info"
+                    className="movies-tag-button-reset"
+                    onClick={() => change(-step, events)}>
+                    &lt;
+                  </Button>
+                  <Button
+                    id="home-btn-right"
+                    bsSize="small"
+                    bsStyle="info"
+                    className="movies-tag-button-reset"
+                    onClick={() => change(step, events)}
+                  >
+                    &gt;
+                  </Button>
+                </ButtonGroup>
+              </Row>
+              <Row className="home-event-panel-header">
+                <Col xs={12}>
+                  <h5 className="home-event-description text-center">
+                    NADCHODZĄCE PROJEKCJE
+                  </h5>
+                </Col>
+              </Row>
+              {events.slice(
+                start, start + step
+              ).map(
+                event => (
+                  <div>
+
+
+                    <Link
+                      key={event.id}
+                      to={"/event/" + event.id}>
+                      <Row className="home-event-panel">
+                        <Col xs={12} key={event.id}>
+                          <h3 className="home-event-title rbc-ellipsis">{event.movieTitle}</h3>
+                          <Row className="home-centered-vert">
+                            <Col xs={3}>
+                              <Image
+                                src={event.moviePicture}
+                                alt={event.movieTitle}
+                                responsive
+                              />
+                            </Col>
+                            <Col
+                              className="home-event-description"
+                              xs={9}>
+                              <h4>
+                                <Glyphicon glyph="time"/> {' '}
+                                {moment(event.start).format('dddd, D MMMM, H:mm')}
                               </h4>
-                              <h6>{event.desc}</h6>
-                            </div>
-                          </Col>
-                        </Row>
-                      </Col>
-                    </Row>
-                  </Link>
-                </div>
-              )
-            )
+                              <h4>
+                                <Glyphicon glyph="map-marker"/>
+                                {' '}
+                                {event.distance} m
+                              </h4>
+                              <div>
+                                <h4 >
+                                  <Glyphicon glyph="bullhorn"/>
+                                  &nbsp;
+                                </h4>
+                                <h6>{event.desc}</h6>
+                              </div>
+                            </Col>
+                          </Row>
+                        </Col>
+                      </Row>
+                    </Link>
+                  </div>
+                )
+              )}
+            </div>
       }
     </Col>
   )
