8b8f21962af8ffb35e2c42a4cfc3ea01d98ec031
Sylwia Rzedzicka <rzedzicka@gmail.com>
2017-03-27 01:55:29 +0200

    FD6GL-79 add tooltips

--- src/components/movies/movies-list.js
+++ src/components/movies/movies-list.js
@@ -1,55 +1,77 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
-import { Button } from 'react-bootstrap'
+import { Button, Tooltip, OverlayTrigger } from 'react-bootstrap'
 import { toggleMovie } from '../../state/userLogin'
 
-const MovieList = ({movies, customTags, query, queryTag, session, user, toggleMovie}) => (
-  <div>
-    {
-      movies.length === 0
-        ?
-        <p>Ładowanie…</p>
-        :
-        movies
-          .filter(movie =>
-            movie.name.toLowerCase().indexOf(query.toLowerCase()) !== -1
-          )
-          .filter(movie =>
-            queryTag > 0 ? movie.tags.indexOf(queryTag) !== -1 : true
-          )
-          .filter(movie =>
-            customTags.every(tag =>
-              movie.tags.indexOf(tag) !== -1
-            )
-          )
-          .map(movie => (
-              <div key={movie.id} className="movies-list-movie col-xs-12 col-sm-6 col-lg-4 movies-no-padding">
-                <div className="movies-spaced-list">
-                  <Link to={'/movie/' + movie.id}>
-                    <img src={movie.pics[0]} alt={movie.name}/>
-                    {movie.name}
-                    <br/>
-                    <span className="movies-original-title">({movie.originalTitle})</span>
-                    {user === null || user.movies.includes(movie.id) ? null
-                      :
-                      < Button bsSize="xsmall" bsStyle="info" className="movies-list-button movies-list-button-add" title="Dodaj do swojej listy" onClick={
-                        (event) => {
-                          event.preventDefault()
-                          return toggleMovie(user.id,
-                            user.movies.includes(movie.id) ? user.movies : user.movies.concat(movie.id),
-                            session.id)
+class MovieList extends React.Component {
+  render() {
+    const {movies, customTags, query, queryTag, session, user, toggleMovie} = this.props
+    const tooltip = (
+      <Tooltip id="tooltip">
+        Dodaj do swojej listy
+      </Tooltip>
+    )
+
+    return (
+      <div>
+        {
+          movies.length === 0
+            ?
+            <p>Ładowanie…</p>
+            :
+            movies
+              .filter(movie =>
+                movie.name.toLowerCase().indexOf(query.toLowerCase()) !== -1
+              )
+              .filter(movie =>
+                queryTag > 0 ? movie.tags.indexOf(queryTag) !== -1 : true
+              )
+              .filter(movie =>
+                customTags.every(tag =>
+                  movie.tags.indexOf(tag) !== -1
+                )
+              )
+              .map(movie => (
+                  <div key={movie.id} className="movies-list-movie col-xs-12 col-sm-6 col-lg-4 movies-no-padding">
+                    <div className="movies-spaced-list">
+                      <Link to={'/movie/' + movie.id}>
+                        <img src={movie.pics[0]} alt={movie.name}/>
+                        {movie.name}
+                        <br/>
+                        <span className="movies-original-title">({movie.originalTitle})</span>
+                        {user === null || user.movies.includes(movie.id) ? null
+                          :
+                          <OverlayTrigger
+                            placement="bottom"
+                            overlay={tooltip}
+                            trigger={['hover']}
+                            delay={100}
+
+                          >
+                          < Button bsSize="xsmall"
+                                   bsStyle="info"
+                                   className="movies-list-button movies-list-button-add"
+                                   onClick={
+                                     (event) => {
+                                       event.preventDefault()
+                                       return toggleMovie(user.id,
+                                         user.movies.includes(movie.id) ? user.movies : user.movies.concat(movie.id),
+                                         session.id)
+                                     }
+                          }>+</Button>
+                          </OverlayTrigger>
                         }
-                      }>+</Button>
-                    }
-                  </Link>
-                </div>
-              </div>
-            )
-          )
-    }
-  </div>
-)
+                      </Link>
+                    </div>
+                  </div>
+                )
+              )
+        }
+      </div>
+    )
+  }
+}
 
 export default connect(
   state => ({
