beab91acd6d6e9693522153a6d00bb9aa53766bf
mitroc <michaltrocha@gmail.com>
2017-04-02 20:13:26 +0200

    Jest tests implementation. Home-filters reducer: change action, slide action - completed.

--- src/components/home/__tests__/home-filters.test.js
+++ src/components/home/__tests__/home-filters.test.js
@@ -22,7 +22,7 @@ describe('home filters reducer', () => {
       value: 1000
     });
 
-    // positive step exceeds event list
+    // positive step exceeds event list length
     expect(
       reducer({
         start: 2,
@@ -33,6 +33,39 @@ describe('home filters reducer', () => {
       value: 1000
     });
 
+    // positive step
+    expect(
+      reducer({
+        start: 2,
+        value: 1000
+      }, change(1, ['foo', 'bar', 'buzz', 'foo', 'bar']))
+    ).toEqual({
+      start: 3,
+      value: 1000
+    });
 
+    // negative step
+    expect(
+      reducer({
+        start: 2,
+        value: 1000
+      }, change(-1, ['foo', 'bar', 'buzz', 'foo', 'bar']))
+    ).toEqual({
+      start: 1,
+      value: 1000
+    });
+
+  });
+
+  test('should handle action: SLIDE', () => {
+    expect(
+      reducer({
+        start: 0,
+        value: 3000
+      }, slide(1000))
+    ).toEqual({
+      start: 0,
+      value: 1000
+    })
   })
 });
